0516317e8d9be4ccd8c7d6db42fcf0616b6184ee
Cyprien Eckert <cipri.eckert@hotmail.fr>
2021-11-22 18:04:01 +0100

    fix category and activity association

--- app/controllers/categories_controller.rb
+++ app/controllers/categories_controller.rb
@@ -5,7 +5,7 @@ class CategoriesController < ApplicationController
 
   def create
     @category = Category.new(category_params)
-    if @category.save?
+    if @category.save
       redirect_to root_path
     else
       render :new
