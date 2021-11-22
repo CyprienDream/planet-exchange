89594b48a820aecb87b7e73c9980ea39d1729045
Cyprien Eckert <cipri.eckert@hotmail.fr>
2021-11-22 17:35:53 +0100

    add new and create actions on activity and category controllers + new views

--- app/controllers/activities_controller.rb
+++ app/controllers/activities_controller.rb
@@ -1,2 +1,20 @@
 class ActivitiesController < ApplicationController
+  def new
+    @activity = Activity.new
+  end
+
+  def create
+    @activity = Activity.new(activity_params)
+    if @activity.save?
+      redirect_to root_path
+    else
+      render :new
+    end
+  end
+
+  private
+
+  def activity_params
+    params.require(:activity).permit(:name)
+  end
 end
