edee98d3712f467eb54165557409f0e5ef9dbb6b
Cyprien Eckert <cipri.eckert@hotmail.fr>
2021-11-23 17:05:37 +0100

    add user controller

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -1,2 +1,8 @@
-class UsersController < ApplicationController
+class UsersController < Devise::RegistrationsController
+  def create
+    super
+    params[:user][:interest_ids].each do |id|
+      InterestUser.create(user: current_user, interest_id: id)
+    end
+  end
 end
--- app/models/interest.rb
+++ app/models/interest.rb
@@ -1,2 +1,4 @@
 class Interest < ApplicationRecord
+  has_many :interest_users
+  has_many :users, through: :interest_users
 end
