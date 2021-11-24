777629e6967fca573016408a08454993f905f909
Maxime Mondet <maxime.mondet@gmail.com>
2021-11-24 15:22:11 +0100

    add javascript for onboarding + install swiper gem

--- app/javascript/components/onboarding.js
+++
@@ -1,5 +0,0 @@
-const btnTag = document.querySelector("#btn-onboarding")
-const slidesTag = document.querySelectorAll(".slides")
-const paginationTag = document.querySelectorAll(".pagination span")
-
-export onBoarding()
--- app/javascript/packs/application.js
+++ app/javascript/packs/application.js
@@ -24,7 +24,7 @@ require("channels")
 
 // External imports
 import "bootstrap";
-import { onBoarding } from '../components/onboarding';
+import { initSwiper } from '../components/swiper';
 
 // Internal imports, e.g:
 // import { initSelect2 } from '../components/init_select2';
@@ -32,5 +32,5 @@ import { onBoarding } from '../components/onboarding';
 document.addEventListener('turbolinks:load', () => {
   // Call your functions here, e.g:
   // initSelect2();
-  onBoarding()
+  initSwiper();
 });
