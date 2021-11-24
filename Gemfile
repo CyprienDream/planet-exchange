550f779883161a5c965a46e62e2bdea0c24ea133
Cyprien Eckert <cipri.eckert@hotmail.fr>
2021-11-24 14:01:24 +0100

    add geolocalisation feature to storage model

--- Gemfile
+++ Gemfile
@@ -64,3 +64,4 @@ gem 'tzinfo-data', platforms: [:mingw, :mswin, :x64_mingw, :jruby]
 
 
 gem 'cloudinary', '~> 1.16.0'
+gem 'geocoder'
--- app/models/storage.rb
+++ app/models/storage.rb
@@ -1,4 +1,6 @@
 class Storage < ApplicationRecord
+  geocoded_by :address
+  after_validation :geocode, if: :will_save_change_to_address?
   belongs_to :user
   has_many :item_storages
   has_many :items, through: :item_storages
