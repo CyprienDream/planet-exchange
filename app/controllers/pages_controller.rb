26aeafb4feb5120cb484fc52d9c3098d03a710b6
Cyprien Eckert <cipri.eckert@hotmail.fr>
2021-11-24 14:38:37 +0100

    add map config

--- app/controllers/pages_controller.rb
+++ app/controllers/pages_controller.rb
@@ -2,4 +2,23 @@ class PagesController < ApplicationController
   def home
     @categories = Category.all
   end
+
+  def search
+    # considering search input is named 'query'
+    if params[:query].present?
+      @items = Item.search_by_name(params[:query]) # yet to be implemented
+    else
+      # display or do something if no search input
+    end
+
+    # define markers and info window for map
+    @markers = @items.geocoded.map do |item|
+      {
+        lat: item.storage.latitude,
+        lng: item.storage.longitude,
+        # info_window: render_to_string(partial: "info_window", locals: { experience: experience })
+        # image_url: helpers.asset_url(experience.photo.key)
+      }
+    end
+  end
 end
