4cad082c76844661db7a06c83b3df56078962ca8
Maxime Mondet <maxime.mondet@gmail.com>
2021-11-24 14:08:11 +0100

    onboarding elements on homepage

---
+++ app/javascript/components/onboarding.js
@@ -0,0 +1,5 @@
+const btnTag = document.querySelector("#btn-onboarding")
+const slidesTag = document.querySelectorAll(".slides")
+const paginationTag = document.querySelectorAll(".pagination span")
+
+export onBoarding()
