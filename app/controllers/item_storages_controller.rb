6d4d1df70051b0c239b96031f304a00c1f32d6d3
Cyprien Eckert <cipri.eckert@hotmail.fr>
2021-11-25 12:05:15 +0100

    add edit logic to item storage controller

--- app/controllers/item_storages_controller.rb
+++ app/controllers/item_storages_controller.rb
@@ -1,3 +1,21 @@
 class ItemStoragesController < ApplicationController
+  def edit
+    @storage = Storage.find(params[:id])
+  end
 
+  def update
+    @item_storage = ItemStorage.find(params[:id])
+    @item_storage.update(item_storage_params)
+    if @item_storage.save
+      redirect_to storage_path(@item_storage.storage)
+    else
+      render :edit
+    end
+  end
+
+  private
+
+  def item_storage_params
+    params.require(:item_storage).permit(:specification)
+  end
 end
