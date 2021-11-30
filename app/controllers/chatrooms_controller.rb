1f1c12f4e0fc2a36878e63ebe2b81c2e3d35dd26
Cyprien Eckert <cipri.eckert@hotmail.fr>
2021-11-30 10:04:13 +0100

    fix displayed name

--- app/controllers/chatrooms_controller.rb
+++ app/controllers/chatrooms_controller.rb
@@ -2,12 +2,15 @@ class ChatroomsController < ApplicationController
   def show
     @chatroom = Chatroom.find(params[:id])
     @message = Message.new
+    @chatroom.users.each do |user|
+      @displayed_name = user.username if user != current_user
+    end
   end
 
   def create
     # do not assign name
     @user = User.find(request.referer.split("/").last.to_i)
-    @chatroom = Chatroom.create(name: "XxXx")
+    @chatroom = Chatroom.create(name: "#{current_user.username} - #{@user.username}")
     @user.chatrooms << @chatroom
     current_user.chatrooms << @chatroom
     redirect_to chatroom_path(@chatroom)
