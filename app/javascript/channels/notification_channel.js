cb06e0d819e5362f306f511f2fde97e9f38118a6
Cyprien Eckert <cipri.eckert@hotmail.fr>
2021-12-01 11:37:18 +0100

    add notification feature for new message

--- app/javascript/channels/notification_channel.js
+++ app/javascript/channels/notification_channel.js
@@ -1,14 +1,15 @@
 import consumer from "./consumer";
 
 const initNotificationCable = () => {
-  console.log("Hi from notif controller");
+  console.log("Notification cable sends its regards.");
   const notificationHolder = document.getElementById('notification-holder');
   if (notificationHolder) {
     const id = notificationHolder.dataset.userId;
     consumer.subscriptions.create({ channel: "NotificationChannel", id: id }, {
       received(data) {
         console.log(data)
-        // notificationHolder.insertAdjacentHTML('beforeend', data);
+        document.getElementById("msg-icon").classList.add("d-none");
+        document.getElementById("active-msg-icon").classList.remove('d-none');
       },
     });
   }
