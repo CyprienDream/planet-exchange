c7901719bc7b76f9d8dde49b19f36136895a6d3d
luciaharcegova <fakeluciamail@gmail.com>
2021-12-01 11:39:18 +0100

    added styling to edit storage

--- app/javascript/components/_form_submit.js
+++ app/javascript/components/_form_submit.js
@@ -1,12 +1,11 @@
 const formSubmit = () => {
+  console.log("FormSubmit sends it's regards, winter is coming");
   const storage = document.querySelector(".storage-magic");
   const checkboxItem = document.querySelectorAll(".checkbox-button");
   storage.addEventListener("click", (e) => {
     const checkboxId = e.target.dataset.checkboxid;
-    console.log(checkboxId);
     if (checkboxId) {
       const formItem = document.getElementById(`${checkboxId}`);
-      console.log(formItem);
       formItem.submit();
     }
   });
