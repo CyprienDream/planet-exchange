098652f398711314b7662cc3c8b256028161710d
Cyprien Eckert <cipri.eckert@hotmail.fr>
2021-12-02 17:02:51 +0100

    merge schema change

--- db/schema.rb
+++ db/schema.rb
@@ -10,8 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2021_12_01_105418) do
-
+ActiveRecord::Schema.define(version: 2021_12_02_153150) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
