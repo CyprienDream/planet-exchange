a734d4fdfee91ce28ed57b095e56d4f630bc6c82
Cyprien Eckert <cipri.eckert@hotmail.fr>
2021-12-17 20:17:58 +0100

    doc: interest model

--- app/models/interest.rb
+++ app/models/interest.rb
@@ -1,6 +1,8 @@
 class Interest < ApplicationRecord
+  # attach cloudinary picture
   has_one_attached :photo
 
+  # define databse relationships
   has_many :interest_users
   has_many :users, through: :interest_users
 end
