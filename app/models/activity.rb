c968ff0419bfc4c19962bdd7ce749feebec7f77b
Cyprien Eckert <cipri.eckert@hotmail.fr>
2021-12-17 20:22:46 +0100

    doc: activity model

--- app/models/activity.rb
+++ app/models/activity.rb
@@ -1,6 +1,9 @@
 class Activity < ApplicationRecord
+  # define databse relationships
   belongs_to :category
   has_many :activity_items
   has_many :items, through: :activity_items
+
+  # attach cloudinary picture
   has_one_attached :photo
 end
