032fed0216336ed4ec6225fbfae8ad0eda011373
Cyprien Eckert <cipri.eckert@hotmail.fr>
2021-12-17 20:16:19 +0100

    doc: item model

--- app/models/item.rb
+++ app/models/item.rb
@@ -1,6 +1,8 @@
 class Item < ApplicationRecord
-  # validates :name, presence: true
+  # attach cloudinary picture
   has_one_attached :photo
+
+  # define databse relationships
   has_many :activity_items
   has_many :activities, through: :activity_items
   has_many :item_storages
@@ -8,6 +10,7 @@ class Item < ApplicationRecord
   has_many :item_materials
   has_many :materials, through: :item_materials
 
+  # define search tool for name field
   include PgSearch::Model
   pg_search_scope :search_by_name,
                   against: [:name],
@@ -17,10 +20,12 @@ class Item < ApplicationRecord
                     }
                   }
 
+  # calculates the volume of an item
   def space_taken
     return ((height * width * length ))
   end
 
+  # calculates the carbon footprint of an item
   def carbon_footprint_calc
     carbon = 0
     self.item_materials.each do |m|
