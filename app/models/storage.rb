31743a92c5af8934ece21e49c33954b3ca7abf7d
Cyprien Eckert <cipri.eckert@hotmail.fr>
2021-12-17 20:13:40 +0100

    doc: storage model

--- app/models/storage.rb
+++ app/models/storage.rb
@@ -1,6 +1,10 @@
 class Storage < ApplicationRecord
+  # add geocoding property to address field
   geocoded_by :address
+  # allow geocoding only if address has the correct format
   after_validation :geocode, if: :will_save_change_to_address?
+
+  # define database relationships
   belongs_to :user
   has_many :item_storages
   has_many :items, through: :item_storages
